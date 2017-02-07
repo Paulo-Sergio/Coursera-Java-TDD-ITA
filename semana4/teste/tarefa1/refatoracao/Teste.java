ef4191eccb04e484f73b308937b0ec22016c4c80
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-02-07 11:38:27 -0300

    realizando atividade de refatoração da semana 4

---
+++ semana4/teste/tarefa1/refatoracao/Teste.java
@@ -0,0 +1,14 @@
+package tarefa1.refatoracao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Teste {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
